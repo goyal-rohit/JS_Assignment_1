131b636fbe43d608da67b63c3ad7c630b6957081
rkgstyles <68886726+rkgstyles@users.noreply.github.com>
2022-08-19 13:21:53 +0530

    2.js

---
+++ 2.js
@@ -0,0 +1,13 @@
+//WAP convert temperatures to and from Celsius, Fahrenheit
+
+//WAP convert temperatures from Celsius to Fahrenheit
+
+let celsius_1 = 60;
+let fahrenheit_1 = (celsius_1*9/5)+32;
+console.log(celsius_1+"C is "+fahrenheit_1+"F");
+
+//WAP convert temperatures from Celsius to Fahrenheit
+
+let fahrenheit_2 = 45;
+let celsius_2 = (fahrenheit_2-32)*5/9;
+console.log(fahrenheit_2+"F is "+celsius_2+"C");
